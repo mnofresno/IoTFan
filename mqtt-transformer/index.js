b73136034edc5b2458df63774daadcf4acad243c
Mariano Fresno <mnofresno@gmail.com>
2021-12-27 11:38:42 -0300

    Add mqtt transformer project

---
+++ mqtt-transformer/index.js
@@ -0,0 +1,32 @@
+const mqtt = require('mqtt');
+const credentials = require('./credentials.json');
+const host = 'mqtt-broker.lan';
+const port = '1883';
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `mqtt://${host}:${port}`
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  reconnectPeriod: 1000,
+  username: credentials.username,
+  password: credentials.password
+})
+
+const topic = '/nodejs/mqtt'
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([topic], () => {
+    console.log(`Subscribe to topic '${topic}'`)
+  })
+  client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+    if (error) {
+      console.error(error)
+    }
+  })
+})
+client.on('message', (topic, payload) => {
+  console.log('Received Message:', topic, payload.toString())
+})
+
