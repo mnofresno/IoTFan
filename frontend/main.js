7e014c1d73471622bd7dc6443a9002ba2036e3cd
mariano.fresno <mnofresno@gmail.com>
2022-01-22 22:32:44 -0300

    Fix frontend buttons style

--- frontend/main.js
+++ frontend/main.js
@@ -1,10 +1,8 @@
 $('document').ready(function() {
   $('#poweron').click(function() {
-    // alert('on');
     $.get('/poweron');
   });
   $('#poweroff').click(function() {
-    // alert('off');
     $.get('/poweroff');
   });
 });
